3d133f9a1d114ff01b5909f487df6d56fbd0127a
Kevin Brummer <kjbrummernsf@gmail.com>
2024-08-31 07:32:23 +0900

    First Commit

---
+++ src/token/token.go
@@ -0,0 +1,70 @@
+package token
+
+type TokenType string
+
+type Token struct{
+	Type TokenType
+	Literal string
+}
+
+const (
+	ILLEGAL = "ILLEGAL"
+	EOF = "EOF"
+
+	// Identifiers and Literals
+
+	IDENT = "IDENT"
+	INT = "INT"
+	IF = "IF"
+	ELSE = "ELSE"
+	RETURN = "RETURN"
+	TRUE = "TRUE"
+	FALSE = "FALSE"
+
+	// Operators
+	
+	ASSIGN = "="
+	PLUS = "+"
+	MINUS = "-"
+	SLASH = "/"
+	ASTERISK = "*"
+	BANG = "!"
+	LT = "<"
+	GT = ">"
+	EQ = "=="
+	NOT_EQ = "!="
+	GT_EQ = ">="
+	LT_EQ = "<="
+
+	// Delimiters
+
+	COMMA = ","
+	SEMICOLON = ";"
+	LPAREN = "("
+	RPAREN = ")"
+	LBRACE = "{"
+	RBRACE = "}"
+
+	// Keywords
+
+	FUNCTION = "FUNCTION"
+	LET = "LET"
+)
+
+var keywords = map[string]TokenType {
+	"fn": FUNCTION,
+	"let": LET,
+	"if": IF,
+	"else": ELSE,
+	"return": RETURN,
+	"true": TRUE,
+	"false": FALSE,
+}
+
+func LookUpIdent(ident string) TokenType {
+	if tok,ok := keywords[ident]; ok {
+		return tok
+	}
+
+	return IDENT
+}
