76a4721640668d22ab3e5a9476c9aaee9aecb1aa
Kevin Brummer <k-brummer@ankh-systems.co.jp>
2024-09-02 17:54:59 +0900

    fix if else String() function

--- src/ast/ast.go
+++ src/ast/ast.go
@@ -219,7 +219,7 @@ func (ie *IfExpression) String() string {
 	out.WriteString(ie.Consequense.String())
 
 	if ie.Alternative != nil {
-		out.WriteString("else")
+		out.WriteString(" else ")
 		out.WriteString(ie.Alternative.String())
 	}
 
