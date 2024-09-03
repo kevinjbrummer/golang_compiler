530475e83563f8022cadedb3c8ba826f00ea4309
Kevin Brummer <k-brummer@ankh-systems.co.jp>
2024-09-03 11:06:05 +0900

    add exit command to repl

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -44,6 +44,10 @@ func Start(in io.Reader, out io.Writer) {
 		}
 
 		line := scanner.Text()
+		if line == "exit" {
+			return
+		}
+		
 		l := lexer.New(line)
 		p := parser.New(l)
 		program := p.ParseProgram()
