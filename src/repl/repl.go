ccf38af8c78453c54576c2e7501934d5c0b06d36
Kevin Brummer <k-brummer@ankh-systems.co.jp>
2024-09-03 10:31:27 +0900

    update repl to evaluate code

--- src/repl/repl.go
+++ src/repl/repl.go
@@ -4,6 +4,7 @@ import (
 	"bufio"
 	"fmt"
 	"goblin/color"
+	"goblin/evaluator"
 	"goblin/lexer"
 	"goblin/parser"
 	"io"
@@ -52,8 +53,12 @@ func Start(in io.Reader, out io.Writer) {
 			continue
 		}
 
-		io.WriteString(out, program.String())
-		io.WriteString(out, "\n")
+		evaluated := evaluator.Eval(program)
+
+		if evaluated != nil {
+			io.WriteString(out, evaluated.Inspect())
+			io.WriteString(out, "\n")
+		}
 	}
 }
 
