f33447120bf696eca7696471bcf1cc30b5440b82
Kevin Brummer <k-brummer@ankh-systems.co.jp>
2024-09-03 16:16:55 +0900

    add functions to evaluator

--- src/object/object.go
+++ src/object/object.go
@@ -1,8 +1,11 @@
 package object
 
 import (
+	"bytes"
 	"fmt"
+	"goblin/ast"
 	"goblin/color"
+	"strings"
 )
 
 type ObjectType string
@@ -12,7 +15,8 @@ const (
 	BOOLEAN_OBJ = "BOOLEAN"
 	NULL_OBJ = "NULL"
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
-	ERROR_OBJECT = "ERROR"
+	ERROR_OBJ = "ERROR"
+	FUNCTION_OBJ = "OBJECT"
 )
 
 type Object interface {
@@ -75,6 +79,34 @@ func (e *Error) Inspect() string {
 }
 
 func (e *Error) Type() ObjectType {
-	return ERROR_OBJECT
+	return ERROR_OBJ
+}
+
+type Function struct {
+	Parameters []*ast.Identifier
+	Body *ast.BlockStatement
+	Env *Environment
+}
+
+func (f *Function) Inspect() string {
+	var out bytes.Buffer
+
+	params := []string{}
+	for _, p := range f.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString("fn")
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") {\n")
+	out.WriteString(f.Body.String())
+	out.WriteString("\n}")
+	
+	return out.String()
+}
+
+func (f *Function) Type() ObjectType {
+	return FUNCTION_OBJ
 }
 
