5532eefb119cb1b704439b21539d27ba66fc01e3
Kevin Brummer <k-brummer@ankh-systems.co.jp>
2024-09-04 09:31:01 +0900

    add builtin functions to evaluator

--- src/object/object.go
+++ src/object/object.go
@@ -9,6 +9,7 @@ import (
 )
 
 type ObjectType string
+type BuiltinFunction func(args ...Object) Object
 
 const (
 	INTEGER_OBJ = "INTEGER"
@@ -18,6 +19,7 @@ const (
 	RETURN_VALUE_OBJ = "RETURN_VALUE"
 	ERROR_OBJ = "ERROR"
 	FUNCTION_OBJ = "OBJECT"
+	BUILTIN_OBJ = "BUILTIN"
 )
 
 type Object interface {
@@ -123,3 +125,14 @@ func (s *String) Type() ObjectType {
 	return STRING_OBJ
 }
 
+type Builtin struct {
+	Fn BuiltinFunction
+}
+
+func(b *Builtin) Inspect() string {
+	return "builtin function"
+}
+
+func(b *Builtin) Type() ObjectType {
+	return BUILTIN_OBJ
+}
