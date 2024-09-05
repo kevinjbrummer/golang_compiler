cb7dd7295d2aa1cf441d8c542b08f50fc9f42a1d
Kevin Brummer <k-brummer@ankh-systems.co.jp>
2024-09-05 11:22:43 +0900

    add macro to parser

--- src/parser/parser_test.go
+++ src/parser/parser_test.go
@@ -827,6 +827,47 @@ func TestParsingIndexExpressions(t *testing.T) {
 		return
 	}
 }
+
+func TestMacroLiteralParsing(t *testing.T) {
+	input := `macro(x, y) { x + y };`
+
+	l := lexer.New(input)
+	p := New(l)
+	program := p.ParseProgram()
+	checkParseErrors(t, p)
+
+	if len(program.Statements) != 1 {
+		t.Fatalf("program.Statements does not include %d statement(s). got=%d", 1, len(program.Statements))
+	}
+
+	stmt, ok := program.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("program.Statements[0] is not *ast.ExpressionStatement. got=%T", program.Statements[0])
+	}
+
+	macro, ok := stmt.Expression.(*ast.MacroLiteral)
+	if !ok {
+		t.Fatalf("stmt.Expression is not *ast.MacroLiteral. got %T", stmt.Expression)
+	}
+
+	if len(macro.Parameters) != 2 {
+		t.Fatalf("macro literal parameters wrong. expected=%d, got=%d\n", 2, len(macro.Parameters))
+	}
+
+	testLiteralExpression(t, macro.Parameters[0], "x")
+	testLiteralExpression(t, macro.Parameters[1], "y")
+
+	if len(macro.Body.Statements) != 1 {
+		t.Fatalf("macro.Body.Statements does not include %d statement(s). got=%d\n", 1, len(macro.Body.Statements))
+	}
+
+	bodyStmt, ok := macro.Body.Statements[0].(*ast.ExpressionStatement)
+	if !ok {
+		t.Fatalf("macro body statemnts is not *ast.ExpressionStatement. got=%T", macro.Body.Statements[0])
+	}
+
+	testInfixExpression(t, bodyStmt.Expression, "x", "+", "y")
+}
 //###############################################
 // Helper Functions 
 //###############################################
